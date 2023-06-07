ec486e7aed5e04c1b7e8a61fc2288df6ea583054
Diana Martinez <d.martinezfrias@ugto.mx>
2023-06-06 20:23:49 -0600

    Corregi conexion php

--- views/courses.php
+++ views/courses.php
@@ -1,1 +1,25 @@
-<!-- Cursos En forma de tarjetas y buscador-->
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    require '../config/conexion.php'; //Pega de manera permanente el codigo
+    $query = "SELECT * FROM cursos";
+    $registro = $conn->prepare($query);
+	$registro->execute();
+	$resultado = $registro->fetch(PDO::FETCH_ASSOC);
+	$curso = null;
+    $curso = $resultado;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nombre del curso: <?= $curso['cur_name']; ?></h1>
+    <h1>Nombre del curso: <?= $curso['cur_descrip']; ?></h1>
+</body>
+</html>
