5bc188f3a4960c19dc2f9205505c4a59c0a76aef
Alexei7845 <ga.garciachaire@ugto.mx>
2023-06-07 22:39:26 -0600

    Cambie diseño de las vistas y buscador, asigne nombre de maestro a los cursos

--- views/courses.php
+++ views/courses.php
@@ -3,10 +3,14 @@
 session_start();
 require '../config/conexion.php'; // Pega de manera permanente el código
 
-$query = "SELECT * FROM cursos"; 
+$query = "SELECT cursos.*, maestros.name AS maestro_name FROM cursos INNER JOIN maestros ON cursos.cur_mae_id = maestros.id"; 
 $registro = $conn->prepare($query);
 $registro->execute();
 $cursos = $registro->fetchAll(PDO::FETCH_ASSOC);
+
+$regist = $conn->prepare($query);
+$regist->execute();
+$maestros = $regist->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -19,6 +23,7 @@ $cursos = $registro->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="../css/courses.css">
     <script type="module" src="../js/courses.js"></script>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -83,26 +88,56 @@ $cursos = $registro->fetchAll(PDO::FETCH_ASSOC);
 
     <section class="home-section">
         <div class="buscador">
-            <input type="text" class="buscStyle" id="buscador" placeholder="Buscar curso por nombre">
+            <input type="text" class="buscStyle" id="buscador" placeholder="Search by name" style="  width: 600px;
+                                                                                                        padding: 10px;
+                                                                                                        border: 2px solid ffffff38;
+                                                                                                        border-width: 1px;
+                                                                                                        border-radius: 14pxpx;
+                                                                                                        background-color: transparent;
+                                                                                                        font-size: 16px;
+                                                                                                        color: #ffffff38;
+                                                                                                        margin-top: 20px;
+                                                                                                        outline: none;">
+            <i class="fa-solid fa-magnifying-glass" style="color: #ffffff; margin-left: 10px; font-size: 20px;"></i>
         </div>
+
+        <h2 class="titulo" style="text-align: center; font-size: 40px; font-weight: 600; color: white; margin-top: 20px;">Courses</h2>
+        <nav class="nav-divider centrado" style=" margin-left: auto;
+                                                margin-right: auto;
+                                                width: fit-content;
+                                                background-color: #30612b;
+                                                height: 5px;
+                                                margin-bottom: 20px;
+                                                width: 90%;
+                                                "></nav>
     <div class="container">
         <div class="card-container">
-            <?php foreach ($cursos as $curso): ?>
-                <div class="card">
+        <?php foreach ($cursos as $index => $curso): ?>
+            <?php $maestro = $maestros[$index]; ?>  
+
+                <div class="card" style="  box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);
+                                            -webkit-box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);
+                                            -moz-box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);
+                                            height:490px;">
                     <div style="flex: 1;">
                         <img class="imgStyle" src="<?= $curso['cur_img'] ?>" style="width: 100%;">
                     </div>
                     <div style="flex: 2; margin-left: 20px;">
                         <div class="card-title">
-                            <span style="font-size: 30px; font-weight: bold;">Course: <?= $curso['cur_name']?></span>
+                            <span style="font-size: 20px; font-weight: bold;"><?= $curso['cur_name']?></span>
+                            <br>
                             <br>
                         </div>
                         <div class="card-body">
-                            <span style="font-size: 20px; font-weight: bold;">Category: <?= $curso['cur_category']?></span>
+                            <label style="font-size: 20px; font-weight: 600;">Category:</label>
+                            <span style="font-size: 15px; font-weight: 500;"><?= $curso['cur_category']?></span>
                             <br>
                             <br>
-                            <span style="font-size: 15px; font-weight: bold;"><?= $curso['cur_descrip']?></span>
+                            <span style="font-size: 15px; font-weight: 500;"><?= $curso['cur_descrip']?></span>
                             <br>
+                            <br>
+                            <label style="font-size: 20px; font-weight: 600;">Teacher:</label>
+                            <span style="font-size: 15px; font-weight: 600;"><?= $curso['maestro_name']?></span>
                         </div>
                     </div>
                 </div>
--- views/students.php
+++ views/students.php
@@ -20,6 +20,7 @@ $maestros = $registro->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="../css/students.css">
     <script type="module" src="../js/students.js"></script>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -82,25 +83,49 @@ $maestros = $registro->fetchAll(PDO::FETCH_ASSOC);
                 </ul>
             </div>
 
-<section class="home-section">
+    <section class="home-section">
     <div class="buscador">
-        <input type="text" class="buscStyle" id="buscador" placeholder="Buscar alumno por nombre">
+        <input type="text" class="buscStyle" id="buscador" placeholder="Search by name" style="  width: 600px;
+                                                                                                        padding: 10px;
+                                                                                                        border: 2px solid ffffff38;
+                                                                                                        border-width: 1px;
+                                                                                                        border-radius: 14pxpx;
+                                                                                                        background-color: transparent;
+                                                                                                        font-size: 16px;
+                                                                                                        color: #ffffff38;
+                                                                                                        margin-top: 20px;
+                                                                                                        outline: none;">
+        <i class="fa-solid fa-magnifying-glass" style="color: #ffffff; margin-left: 10px; font-size: 20px;"></i>
     </div>
+
+    <h2 class="titulo" style="text-align: center; font-size: 40px; font-weight: 600; color: white; margin-top: 20px;">Students</h2>
+    <nav class="nav-divider centrado" style="   margin-left: auto;
+                                                margin-right: auto;
+                                                width: fit-content;
+                                                background-color: #30612b;
+                                                height: 5px;
+                                                margin-bottom: 20px;
+                                                width: 90%;
+                                                "></nav>
     <div class="container">
         <div class="card-container">
             <?php foreach ($maestros as $maestro): ?>
                 <?php if (intval($maestro['verify']) === 0): ?>
-                    <div class="card">
+                    <div class="card" style="  box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);
+                                                -webkit-box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);
+                                                -moz-box-shadow: 1px 0px 12px 4px rgba(0,0,0,0.49);">
                         <div style="flex: 1;">
                             <img class="imgStyle" src="<?= $maestro['mae_img'] ?>">
                         </div>
                         <div style="flex: 2; margin-left: 20px;">
                             <div class="card-title">
-                                <span style="font-size: 30px; font-weight: bold;">Name: <?= $maestro['name']?></span>
+                                <label style="font-size: 20px; font-weight: 600;">Name:</label>
+                                <span style="font-size: 15px; font-weight: 500;"><?= $maestro['name']?></span>
                                 <br>
                             </div>
                             <div class="card-body">
-                                <span style="font-size: 20px; font-weight: bold;">Email: <?= $maestro['email']?></span>
+                                <label style="font-size: 20px; font-weight: 600;">Email:</label>
+                                <span style="font-size: 15px; font-weight: 500;"><?= $maestro['email']?></span>
                                 <br>
                             </div>
                         </div>
